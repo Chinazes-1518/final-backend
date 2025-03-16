03d1e85b70b9bd6450ab5b9ebf8c0ebfe3c34b01
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-03-16 12:55:02 +0300

    !!

--- API_work/full2json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('FULL.txt', 'r') as f:
-    jj = []
-    values = []
-    for line in f.read().splitlines():
-        values.append(list(map(int, line.split('\t'))))
-    with open('FULL.json', 'w') as f:
-        json.dump(values, f)
--- routes/test.py
+++ routes/test.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy import select, update, insert, delete
+import json
 
 import database
 import utils
@@ -8,22 +9,24 @@ import utils
 router = APIRouter(prefix='/test')
 
 
-@router.get('/get')
+@router.get('/get_map')
 async def get() -> JSONResponse:
-    with database.sessions.begin() as session:
-        req = session.execute(select(database.User))
-        return utils.json_responce(req.scalars().all())
+    # with database.sessions.begin() as session:
+    #     req = session.execute(select(database.User))
+    #     return utils.json_responce(req.scalars().all())
+    with open('map.json', 'r') as f:
+        return utils.json_responce(json.load(f))
 
 
-@router.get('/add')
-async def add(name: str, age: int) -> JSONResponse:
-    with database.sessions.begin() as session:
-        req = session.execute(insert(database.User).values(name=name, age=age))
-        return utils.json_responce({'z': 'v'})
+# @router.get('/add')
+# async def add(name: str, age: int) -> JSONResponse:
+#     with database.sessions.begin() as session:
+#         req = session.execute(insert(database.User).values(name=name, age=age))
+#         return utils.json_responce({'z': 'v'})
 
 
-@router.get('/delete')
-async def deleteasdsadasd(id: int) -> JSONResponse:
-    with database.sessions.begin() as session:
-        req = session.execute(delete(database.User).where(database.User.id == id))
-        return utils.json_responce({'z': 'v'})
+# @router.get('/delete')
+# async def deleteasdsadasd(id: int) -> JSONResponse:
+#     with database.sessions.begin() as session:
+#         req = session.execute(delete(database.User).where(database.User.id == id))
+#         return utils.json_responce({'z': 'v'})
