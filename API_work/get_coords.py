f05e23b581ee0f5c60d01aec7dfdd09b4d3496f0
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2025-03-16 19:20:54 +0300

    Update get_coords.py

--- API_work/get_coords.py
+++ API_work/get_coords.py
@@ -5,9 +5,6 @@ import pathlib
 
 def run(url):
     data = requests.get(url).json()['message']
-    data['sender'][0] -= 3
-    data['sender'][1] -= 3
-    data['listener'][0] -= 3
-    data['listener'][1] -= 3
+
     with open(pathlib.Path(__file__).parent.parent.joinpath('coords.json'), 'w') as f:
         json.dump(data, f)
